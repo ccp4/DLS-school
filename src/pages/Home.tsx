5b760482beeb08583b000754edb34799190e2fd5
David Waterman <dgwaterman@gmail.com>
2025-06-04 09:55:42 +0100

    improve text

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,49 +12,48 @@ export function Home() {
         </p>
       </div>
       <img
-        src={`${import.meta.env.BASE_URL}panorama.jpg`}
+        src={`${import.meta.env.BASE_URL}Diamond_aerial.jpg`}
         alt=""
         className="min-h-40 max-w-full object-cover"
       />
       <p>
-        <b>We are very pleased to announce the twelfth joint Diamond-CCP4 MX workshop for graduate students, postdocs and early career scientists.</b>
+        <b>
+          We are very pleased to announce the twelfth joint Diamond-CCP4 MX
+          workshop for graduate students, postdocs and early career scientists.
+        </b>
       </p>
-    <p>
-      All aspects of structure solution will be covered during the
-      workshop, from data collection through to phasing, refinement,
-      validation and deposition. Please see the
-        preliminary <InternalLink to="programme" text="programme" /> for more
-        details.
-    </p>
-    <List>
-      <li>
-        Lectures and tutorials will be delivered by experts in the field,
-        sometimes even software authors themselves!
-      </li>
-      <li>
-        One day of data collection time will be provided at Diamond's excellent MX beamlines
-      </li>
-      <li>
-        You will be able to work alongside world-leading scientists
-        and methods developers on your own projects.
-      </li>
-
-    </List>
-    <p>
-      The workshop will begin with three days of lectures delivered remotely
-      by Zoom. Following that, further lectures, tutorials, data
-      collection and subsequent analysis will be done in person at Diamond Light
-      Source, on the Harwell Oxford campus in Oxfordshire, UK.
-    </p>
+      <p>
+        All aspects of structure solution will be covered during the workshop,
+        from data collection through to phasing, refinement, validation and
+        deposition. Please see the preliminary{" "}
+        <InternalLink to="programme" text="programme" /> for more details.
+      </p>
+      <List>
+        <li>
+          Lectures and tutorials will be delivered by experts in the field,
+          sometimes even software authors themselves!
+        </li>
+        <li>
+          One day of data collection time will be provided at Diamond's
+          excellent MX beamlines
+        </li>
+        <li>
+          You will be able to work alongside world-leading scientists and
+          methods developers on your own projects.
+        </li>
+      </List>
+      <p>
+        The workshop will begin with three days of lectures delivered remotely
+        by Zoom. Following that, further lectures, tutorials, data collection
+        and subsequent analysis will be done in person at Diamond Light Source,
+        on the Harwell Oxford campus in Oxfordshire, UK.
+      </p>
       <InternalLink to="location" text="More details and directions" />
       <p>
         Spaces are limited. The applicant will have to demonstrate the need for
         this sort of specialist training, and be prepared to submit the e-mail
         address of a supervisor who will write a letter of recommendation in
         support of their application.
-      </p>
-      <p>
-        The organisers.
       </p>
     </main>
   );
